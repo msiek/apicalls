9a920c212426b43c6ff9ebfb0ae46bc5f1f9aa91
Jerry Harrison <jerry@indie.pw>
2018-01-08 13:18:42 -0500

    cleaned up formatting, removed unused and unneeded libs.

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-require('request')
-const index         =  require('./routes').router
-const express       =  require('express');
-const app           =  express();
-const port          =  process.env.PORT || 8080;
+const express = require('express');
+const index = require('./routes').router;
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.use('/', index)
-
+app.use('/', index);
 app.listen(port);
-console.log('Server Starts on Port: ' + port);
+
+console.log(`Server started and listening on http://localhost:${port}`);

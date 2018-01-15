c4ec9dbf83f0d5fc3a7b430138130f4d4d316d31
msiek <michael_siek123@hotmail.com>
2018-01-15 09:34:55 -0500

    4sq with geocoding

---
+++ utils/test.js
@@ -0,0 +1,97 @@
+const apiKey = 'AIzaSyD9k0r4o0XKwWQgPgL5r1eX1dvFJt2Zxyw';
+const NodeGeocoder = require('node-geocoder');
+const options = {
+    provider: 'google',
+    httpAdapter: 'https',
+    apiKey: apiKey
+};
+const geocoder = NodeGeocoder(options)
+
+require('request');
+const request = require('request-promise-native');
+const qs = require('querystring');
+const baseFourSqURL = 'https://api.foursquare.com/v2/venues';
+
+const forsquareParams = {
+    client_id: 'GRGLBQDIY40TNUGKHJAPFOJAV0ZQF1ZZ1ASAGU14C4VGQI2Z',
+    client_secret: 'ZOEYJUIIE2BT0DMFPYXHIBB0M0XTJ33ILIANKPIH4BSKCSWR',
+    query: 'wafflehouse',
+    v: '20170801',
+    limit: 10
+};
+/*
+exports.waffleHouseLookup = function (lat, long) {
+    const params = qs.stringify(Object.assign({
+        ll: `${lat},${long}`
+    }, forsquareParams));
+
+    const url = `${baseFourSqURL}/search?${params}`;
+
+    return new Promise((resolve, reject) => {
+        request.get(url)
+            .then(body => {
+                try {
+                    return JSON.parse(body).response.venues.map(venue => {
+                        console.log(venue.location.lat,venue.location.lng)
+                        return {
+                            name: venue.name,
+                            address: {
+                                google
+                            },
+                            phone: venue.contact.formattedPhone,
+                            distance: ( venue.location.distance / 1609.344 ).toFixed(2),
+                            meta: {
+                                //...venue
+                            }
+                        };
+                    });
+                } catch (err) {
+                    return reject({
+                        url,
+                        error: err,
+                    });
+                }
+            })
+            .then(resolve)
+            .catch(err => {
+                return reject({
+                    url,
+                    error: err,
+                })
+            });
+    });
+};
+*/
+
+function wf(lat, long){
+    geocoder.reverse({lat:lat, lon:long})
+        .then((res)=>{
+            formattedAddress = res[0].formattedAddress
+            streetNumber = res[0].streetNumber
+            streetName = res[0].streetName
+            la = res[0].latitude.toFixed(6)
+            lng = res[0].longitude.toFixed(6)
+            cityName = res[0].city
+            postalCode = res[0].zipcode
+            country = res[0].country
+            state = res[0].administrativeLevels.level1short
+            countryCode = res[0].countryCode
+            addr = {
+                address: `${streetNumber} ${streetName}`,
+                postalcode: `${postalCode}`,
+                city: `${cityName}`,
+                state: `${state}`,
+                country: `${country}`,
+                country_code: `${countryCode}`,
+                formatted_address: `${formattedAddress}`,
+                lat: `${la}`,
+                lng: `${lng}`
+            }
+            console.log(addr)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+}
+
+wf(33.70811738854921, -78.91176692003853)
